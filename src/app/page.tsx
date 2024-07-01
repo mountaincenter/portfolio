4a2db18ed08ea591a0db1433e12bb0b3ba4cf6ad
mountaincenter <y.hiroyuki560730@gmail.com>
2024-07-01 13:53:23 +0900

    Implemented dark mode using shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import { CreatePost } from "@/app/_components/create-post";
 import { getServerAuthSession } from "@/server/auth";
 import { api } from "@/trpc/server";
 
+import { ModeToggle } from "./_components/ModeToggle/ModeToggle";
+
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
   const session = await getServerAuthSession();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center ">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
@@ -53,6 +55,7 @@ export default async function Home() {
             >
               {session ? "Sign out" : "Sign in"}
             </Link>
+            <ModeToggle />
           </div>
         </div>
 
